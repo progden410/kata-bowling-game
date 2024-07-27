599f9883af7019ac7de4e4e25724d2fcebe26712
progden <ccdrumer@gmail.com>
2024-07-27 23:31:52 +0800

    Add Log4j dependency to pom.xml
    Introduced Log4j API to enable robust logging capabilities. This addition will help in better monitoring and debugging of the application by providing comprehensive logging functionality.

--- src/main/java/io/kata/bowlinggame/ErrorFrame.java
+++ src/main/java/io/kata/bowlinggame/ErrorFrame.java
@@ -4,4 +4,14 @@ public class ErrorFrame extends Frame {
     public ErrorFrame(int frameNum) {
         super(frameNum);
     }
+
+    @Override
+    public void roll(int pin) {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBonus(int pins) {
+        throw new java.lang.UnsupportedOperationException();
+    }
 }
--- src/main/java/io/kata/bowlinggame/TenthFrame.java
+++ src/main/java/io/kata/bowlinggame/TenthFrame.java
@@ -4,4 +4,14 @@ public class TenthFrame extends Frame {
     public TenthFrame(int frameNum) {
         super(frameNum);
     }
+
+    @Override
+    public void roll(int pin) {
+        if (this.bonusType() != BonusType.Normal && pins.size() < 3) {
+            pins.add(pin);
+        } else if (this.bonusType() == BonusType.Normal && pins.size() < 2) {
+            pins.add(pin);
+        }
+
+    }
 }
