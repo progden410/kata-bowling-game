b0f8082c8f1c8d80fdf0643be933e5f984e32f75
progden <ccdrumer@gmail.com>
2024-07-27 23:53:22 +0800

    Add logging and refactor access modifiers for Frame and TenthFrame
    Introduced Log4j logging to the Frame and TenthFrame classes to enhance debugging. Additionally, modified access modifiers to increase subclass flexibility and ensure correct bonus frame processing.

--- src/main/java/io/kata/bowlinggame/Frame.java
+++ src/main/java/io/kata/bowlinggame/Frame.java
@@ -1,11 +1,16 @@
 package io.kata.bowlinggame;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class Frame {
+    private static final Logger logger = LogManager.getLogger(Frame.class);
+
     public static Frame of(int frameNum) {
-        if (1 <= frameNum && frameNum <= 10) {
+        if (1 <= frameNum && frameNum < 10) {
             return new Frame(frameNum);
         } else if (frameNum == 10) {
             return new TenthFrame(10);
@@ -13,11 +18,11 @@ public class Frame {
         return new ErrorFrame(frameNum);
     }
 
-    private final int frameNum;
+    protected final int frameNum;
     List<Integer> pins = new LinkedList<>();
     List<Integer> bonus = new LinkedList<>();
 
-    private BonusType bonusType = BonusType.Normal;
+    protected BonusType bonusType = BonusType.Normal;
 
     protected Frame(int frameNum) {
         this.frameNum = frameNum;
@@ -31,9 +36,10 @@ public class Frame {
     public void roll(int pin) {
         pins.add(pin);
         setBounsType();
+        logger.debug(this);
     }
 
-    private void setBounsType() {
+    protected void setBounsType() {
         bonusType = isStrike() ? BonusType.Strike :
                 isSpare() ? BonusType.Spare :
                         BonusType.Normal;
--- src/main/java/io/kata/bowlinggame/TenthFrame.java
+++ src/main/java/io/kata/bowlinggame/TenthFrame.java
@@ -1,17 +1,35 @@
 package io.kata.bowlinggame;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.EnumSet;
+
 public class TenthFrame extends Frame {
+    private static final Logger logger = LogManager.getLogger(TenthFrame.class);
+
     public TenthFrame(int frameNum) {
         super(frameNum);
     }
 
     @Override
     public void roll(int pin) {
-        if (this.bonusType() != BonusType.Normal && pins.size() < 3) {
-            pins.add(pin);
-        } else if (this.bonusType() == BonusType.Normal && pins.size() < 2) {
+        logger.debug(this);
+        if (this.bonusType() == BonusType.Normal && pins.size() < 2) {
+            super.roll(pin);
+        } else if (EnumSet.of(BonusType.Spare, BonusType.Strike).contains(this.bonusType) && pins.size() < 3) {
+            // 只要有 spare 或 strike 就可以打到三球
+            // 這時候不用在 setBonusType，不須要再改狀態了
+            // 如果改了會回到 normal 會吃不到第三球
             pins.add(pin);
         }
+    }
 
+    @Override
+    public String toString() {
+        return "TenthFrame" +
+                "[" + frameNum + "][" + bonusType + "] = " + score() +
+                ", pins=" + pins +
+                ", bonus=" + bonus;
     }
 }
